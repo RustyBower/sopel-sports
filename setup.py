a4f2b93e045a0f4f420d9b09276c479804a7c6ee
Rusty Bower <rbower@riotgames.com>
2019-12-27 14:15:03 -0500

    Bump version: 1.1.2 → 1.1.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('dev-requirements.txt') as dev_requirements_file:
 
 setup(
     name='sopel_modules.sports',
-    version='1.1.2',
+    version='1.1.3',
     description='Sopel Sports Module',
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/x-rst',
